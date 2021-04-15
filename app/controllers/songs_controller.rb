49f450fb503d602210f657eafc9de44cc5b040b2
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-04-15 18:20:48 +0200

    songs index limit

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,7 +4,7 @@ class SongsController < ApplicationController
   def index
     require 'will_paginate/array'
     @songs_total = Song.all.count
-    @songs = Song.all.sort.reverse.paginate(page: params[:page], per_page: 50)
+    @songs = Song.all.limit(200).sort.reverse.paginate(page: params[:page], per_page: 50)
   end
 
   def originals
